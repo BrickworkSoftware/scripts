721c6365b087920c48f14fdd1a76070ec5b62540
Saadiq Rodgers-King <saadiq@gmail.com>
2016-04-16 23:26:29 -0400

    posting successfully to sprintly with a subset of ticket data

--- asana2sprintly.rb
+++ asana2sprintly.rb
@@ -8,29 +8,10 @@ require 'net/https'
 
 conf = YAML::load_file('config.yaml')
 
-puts conf
-puts "Asana Token: #{conf['asana_token']}"
-puts "Sprintly Key: #{conf['sprintly_api_key']}"
-puts "Sprintly Email: #{conf['sprintly_email']}"
-
-@toSend = {
-    "date" => "2012-07-02",
-    "aaaa" => "bbbbb",
-    "cccc" => "dddd"
-}.to_json
-
-uri = URI.parse("https://sprint.ly/api/user/whoami.json")
-https = Net::HTTP.new(uri.host,uri.port)
-https.use_ssl = true
-req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-req['foo'] = 'bar'
-req.basic_auth conf['sprintly_email'], conf['sprintly_api_key']
-req.body = "[ #{@toSend} ]"
-res = https.request(req)
-puts "Response #{res.code} #{res.message}: #{res.body}"
-
-
-exit
+# puts conf
+# puts "Asana Token: #{conf['asana_token']}"
+# puts "Sprintly Key: #{conf['sprintly_api_key']}"
+# puts "Sprintly Email: #{conf['sprintly_email']}"
 
 client = Asana::Client.new do |c|
   c.authentication :access_token, conf['asana_token']
@@ -49,21 +30,37 @@ task = Asana::Task.find_by_id(client, '112974848331541')
 #
 # puts task
 
-attachments = []
-
-puts attachments
-Asana::Attachment.find_by_task(client, task: task.id).each { |a|
-  #puts a
-  attachments.push(Asana::Attachment.find_by_id(client, a.id))
-  #puts attachments
-}
-
-puts "Attachments #{attachments}"
-#puts sm_attachment
-
-#lg_attachment = Asana::Attachment.find_by_id(client, sm_attachment.id)
-
-#puts lg_attachment
+# attachments = []
+# Asana::Attachment.find_by_task(client, task: task.id).each { |a|
+#   #puts a
+#   attachments.push(Asana::Attachment.find_by_id(client, a.id))
+#   #puts attachments
+# }
+# puts "Attachments #{attachments}"
 
 # curl -u SPRINTLY_EMAIL:API_KEY https://sprint.ly/api/products.json
 # https://sprint.ly/api/user/whoami.json
+
+
+@toSend = {
+  "type" => "task",
+  "title" => task.name,
+  "description" => task.notes
+}#.to_json
+
+# /api/products/{product_id}/items.json
+# test product id: 40942
+# uri = URI.parse("https://sprint.ly/api/user/whoami.json")
+uri = URI.parse("https://sprint.ly/api/products/40942/items.json")
+# uri = URI.parse("https://httpbin.org/post")
+https = Net::HTTP.new(uri.host,uri.port)
+https.use_ssl = true
+# req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/x-www-form-urlencoded'})
+req['foo'] = 'bar'
+req.basic_auth conf['sprintly_email'], conf['sprintly_api_key']
+req.body = "[ #{@toSend} ]"
+req.body = URI.encode_www_form(@toSend)
+puts req.body
+res = https.request(req)
+puts "Response #{res.code} #{res.message}: #{res.body}"
