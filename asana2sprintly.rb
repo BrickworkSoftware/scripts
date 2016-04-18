424014f2b84404c84f9daf4533d3eaf577a2f6d4
Saadiq Rodgers-King <saadiq@gmail.com>
2016-04-17 22:26:45 -0400

    reads asana ticket IDs from STDIN

--- asana2sprintly.rb
+++ asana2sprintly.rb
@@ -5,64 +5,68 @@ require 'yaml'
 require 'uri'
 require 'net/http'
 require 'net/https'
+require 'json'
 
 conf = YAML::load_file('config.yaml')
 
-# puts conf
-# puts "Asana Token: #{conf['asana_token']}"
-# puts "Sprintly Key: #{conf['sprintly_api_key']}"
-# puts "Sprintly Email: #{conf['sprintly_email']}"
+tix = {}
+re = /(https?:\/\/app.asana.com\/\d\/\d+\/(\d+)(\/f)?)/
+ARGF.each { |line|
+  if matches = line.scan(re)
+    matches.each{ |match|
+      tix[match[1]] = match[0]
+    }
+  end
+}
+
+# using the hash for uniqueness to make sure I don't dupe tickets
+# in a single run
 
 client = Asana::Client.new do |c|
   c.authentication :access_token, conf['asana_token']
 end
 
-#client.workspaces.find_all.first
+tix.keys.each { |aID|
+  task = Asana::Task.find_by_id(client, aID)
 
-# brickwork workspace
-#workspace = client.workspaces.find_by_id(15547030788476)
+  tags = ""
+  task.tags.each { |t|
+    tags << t.name << ","
+  }
+  tags.chop!
 
-task = Asana::Task.find_by_id(client, '112974848331541')
-# puts task.name
-# puts task.notes
-# puts task.tags
+  # attachments = []
+  # Asana::Attachment.find_by_task(client, task: task.id).each { |a|
+  #   #puts a
+  #   attachments.push(Asana::Attachment.find_by_id(client, a.id))
+  #   #puts attachments
+  # }
+  # puts "Attachments #{attachments}"
 
-tags = ""
-task.tags.each { |t|
-  tags << t.name << ","
+  @toSend = {
+    "type" => "task",
+    "title" => task.name,
+    "description" => task.notes + "\n\nSource: #{tix[aID]}",
+    "tags" => tags
+  }#.to_json
+
+  # uri = URI.parse("https://sprint.ly/api/user/whoami.json")
+  uri = URI.parse("https://sprint.ly/api/products/#{conf['sprintly_product_id']}/items.json")
+  # uri = URI.parse("https://httpbin.org/post")
+  https = Net::HTTP.new(uri.host,uri.port)
+  https.use_ssl = true
+  # req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+  req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/x-www-form-urlencoded'})
+  # req['foo'] = 'bar'
+  req.basic_auth conf['sprintly_email'], conf['sprintly_api_key']
+  #req.body = "[ #{@toSend} ]"
+  req.body = URI.encode_www_form(@toSend)
+  #puts req.body
+  res = https.request(req)
+
+  if res.code == "200"
+    puts "#{tix[aID]} => #{JSON.parse(res.body)['short_url']}"
+  else
+    puts "Response #{res.code} #{res.message}: #{res.body}"
+  end
 }
-tags.chop!
-
-# puts task.id
-#
-# puts task
-
-# attachments = []
-# Asana::Attachment.find_by_task(client, task: task.id).each { |a|
-#   #puts a
-#   attachments.push(Asana::Attachment.find_by_id(client, a.id))
-#   #puts attachments
-# }
-# puts "Attachments #{attachments}"
-
-@toSend = {
-  "type" => "task",
-  "title" => task.name,
-  "description" => task.notes,
-  "tags" => tags
-}#.to_json
-
-# uri = URI.parse("https://sprint.ly/api/user/whoami.json")
-uri = URI.parse("https://sprint.ly/api/products/#{conf['sprintly_product_id']}/items.json")
-# uri = URI.parse("https://httpbin.org/post")
-https = Net::HTTP.new(uri.host,uri.port)
-https.use_ssl = true
-# req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/x-www-form-urlencoded'})
-# req['foo'] = 'bar'
-req.basic_auth conf['sprintly_email'], conf['sprintly_api_key']
-#req.body = "[ #{@toSend} ]"
-req.body = URI.encode_www_form(@toSend)
-puts req.body
-res = https.request(req)
-puts "Response #{res.code} #{res.message}: #{res.body}"
