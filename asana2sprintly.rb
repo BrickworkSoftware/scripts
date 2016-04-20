36d97610fc662f15d59e1f08199047d0020430f8
Saadiq Rodgers-King <saadiq@gmail.com>
2016-04-19 20:05:18 -0400

    refactoring into functions for readability

--- asana2sprintly.rb
+++ asana2sprintly.rb
@@ -7,7 +7,39 @@ require 'net/http'
 require 'net/https'
 require 'json'
 
-conf = YAML::load_file('config.yml')
+@conf = YAML::load_file('config.yml')
+
+def get_attachments_by_id( client, id)
+  files = []
+  Asana::Attachment.find_by_task(client, task: id).each { |a|
+    files.push(Asana::Attachment.find_by_id(client, a.id))
+  }
+  return files
+end
+
+def create_sprintly_ticket( type, status, name, desc, tags, asana_url)
+  ticket = {
+    "type" => type,
+    "status" => status,
+    "title" => name,
+    "description" => desc + "\n\nSource: #{asana_url}",
+    "tags" => tags
+  }#.to_json
+
+  # uri = URI.parse("https://sprint.ly/api/user/whoami.json")
+  uri = URI.parse("https://sprint.ly/api/products/#{@conf['sprintly_product_id']}/items.json")
+  # uri = URI.parse("https://httpbin.org/post")
+  https = Net::HTTP.new(uri.host,uri.port)
+  https.use_ssl = true
+  # req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+  req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/x-www-form-urlencoded'})
+  # req['foo'] = 'bar'
+  req.basic_auth @conf['sprintly_email'], @conf['sprintly_api_key']
+  #req.body = "[ #{@toSend} ]"
+  req.body = URI.encode_www_form(ticket)
+  #puts req.body
+  return https.request(req)
+end
 
 # pop these args off so ARGF doesn't try
 # to read them as files
@@ -28,8 +60,9 @@ ARGF.each { |line|
   end
 }
 
+# Asana module docs: http://www.rubydoc.info/github/Asana/ruby-asana/master/Asana
 client = Asana::Client.new do |c|
-  c.authentication :access_token, conf['asana_token']
+  c.authentication :access_token, @conf['asana_token']
 end
 
 tix.keys.each { |aID|
@@ -44,35 +77,9 @@ tix.keys.each { |aID|
   }
   tags.chop!
 
-  # attachments = []
-  # Asana::Attachment.find_by_task(client, task: task.id).each { |a|
-  #   #puts a
-  #   attachments.push(Asana::Attachment.find_by_id(client, a.id))
-  #   #puts attachments
-  # }
-  # puts "Attachments #{attachments}"
+  attachments = get_attachments_by_id( client, task.id)
 
-  @toSend = {
-    "type" => "task",
-    "status" => "backlog",
-    "title" => task.name,
-    "description" => task.notes + "\n\nSource: #{tix[aID]}",
-    "tags" => tags
-  }#.to_json
-
-  # uri = URI.parse("https://sprint.ly/api/user/whoami.json")
-  uri = URI.parse("https://sprint.ly/api/products/#{conf['sprintly_product_id']}/items.json")
-  # uri = URI.parse("https://httpbin.org/post")
-  https = Net::HTTP.new(uri.host,uri.port)
-  https.use_ssl = true
-  # req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-  req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/x-www-form-urlencoded'})
-  # req['foo'] = 'bar'
-  req.basic_auth conf['sprintly_email'], conf['sprintly_api_key']
-  #req.body = "[ #{@toSend} ]"
-  req.body = URI.encode_www_form(@toSend)
-  #puts req.body
-  res = https.request(req)
+  res = create_sprintly_ticket( "task", "backlog", task.name, task.notes, tags, tix[aID])
 
   if res.code == "200"
     puts "#{tix[aID]} => #{JSON.parse(res.body)['short_url']}"
