35a8bb910b04676a3d00762998758dd86817d72d
Saadiq Rodgers-King <saadiq@gmail.com>
2016-11-10 20:03:49 -0500

    final mile updates and some flourishes
    - Can color output but need to edit script to turn on and off
    - Cycles through all files passed in
    - Outputs csv of input including success or failure status with error
    message

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'asana'
 gem 'faraday'
+gem 'colorize'
--- special_hours_update.rb
+++ special_hours_update.rb
@@ -4,55 +4,74 @@ require 'yaml'
 require 'json'
 require 'csv'
 require 'faraday'
+require 'colorize'
 
 @conf = YAML::load_file('config.yml')
+String.disable_colorization = true
 
-csv_file, domain, api_key, *the_rest = ARGV
+domain, api_key, *file_list = ARGV
 
 conn = Faraday.new(url: "https://#{domain}.brickworksoftware.com") # create a new Connection with base URL
+# puts conn.url_prefix
 
-# {
-#     "special_hour": {
-#         "date": "2016-06-12",
-#         "start_time": "9am",
-#         "display_name": "Renovations",
-#         "end_time": "9pm",
-#         "closed": false,
-#         "day_of_week": 0
-#     }
-# }
+#String.color_samples
 
-CSV.foreach(csv_file) do |row|
-  puts row.to_json
-  next if row[0] == "store_number" # skip header line
+errors = 0
+lines = 0
+file_list.each { |file|
+  CSV.foreach(file) do |row|
+    #puts row.to_json
+    next if row[0].nil? || row[0].empty? # skip header line
 
-  hour = {
-    "special_hour"=> {
-      "date"=> row[2],
-      "start_time"=> row[3],
-      "display_name"=> row[5],
-      "end_time"=> row[4],
-      "closed"=> row[6],
-      "day_of_week"=> 0 # why the fuck do I need day of the week?
+    closed = ""
+    if !row[5].nil? && !row[5].empty?
+      closed = row[5].downcase == "yes" || row[5].downcase == "true"
+    end
+
+    hour = {
+      "special_hour"=> {
+        "date"=> row[1],
+        "start_time"=> row[2],
+        "end_time"=> row[3],
+        "display_name"=> row[4],
+        "closed"=> closed
+      }
     }
-  }
 
-  puts hour.to_json
-  exit
+    status = Array(2)
+    color = :white
+    if row[0] != "store_number" # don't submit header labels
+      # puts JSON.pretty_generate(hour).colorize(:yellow)
 
-  url = "/api/v3/admin/stores/#{row[0]}/special_hours"
-  puts url
-  res = conn_c.post do |req|
-    req.url url
-    req.params['api_key'] = api_key
-    req.params['store_number'] = "true"
-    req.headers['Content-Type'] = 'application/json'
-    req.body = hour.to_json
+      url = "/api/v3/admin/stores/#{row[0]}/special_hours"
+      # puts url
+      res = conn.post do |req|
+        req.url url
+        req.params['api_key'] = api_key
+        req.params['store_number'] = "true"
+        req.headers['Content-Type'] = 'application/json'
+        req.body = hour.to_json
+      end
+
+      lines += 1
+
+      if res.status.to_i >= 200 && res.status.to_i < 300
+        color = :green
+        status = [ 'SUCCESS', '']
+      else
+        color = :red
+        status = [ 'FAILURE', res.body]
+        errors += 1
+      end
+    else
+      color = :white
+      status = ['result', 'error']
+    end
+
+    print (row + status).to_csv.colorize(color)
+
   end
+}
 
-  puts res.status
-  puts res.body
-
-  # puts res.status
-  #sprintly = JSON.parse(res.body)
-end
+puts "\nLines Processed: #{lines}".colorize(:cyan)
+puts "Total Error Rows: #{errors}".colorize(:red)
