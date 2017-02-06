c7a723b92653ec9b5ac0d6778170e168df64fbd2
Saadiq Rodgers-King <saadiq@gmail.com>
2017-02-06 17:50:50 -0500

    refactored pdf generation to create cards for taskless stories

--- pivotal_task_cards.rb
+++ pivotal_task_cards.rb
@@ -8,7 +8,6 @@ require 'prawn/icon'
 
 @conf = YAML::load_file('config.yml')
 
-font_type = "Helvetica"
 icons = [ 'fa-shopping-bag', 'fa-ticket', 'fa-map-pin', 'fa-rocket',
   'fa-car', 'fa-anchor', 'fa-camera', 'fa-bullhorn', 'fa-bullseye',
   'fa-asterisk', 'fa-bicycle', 'fa-cloud', 'fa-diamond', 'fa-flag',
@@ -25,42 +24,45 @@ icons.shuffle! # don't want to get bored with the icons
 card_pic = icons[0]
 
 def gen_card(pdf, icon, story, task, estimate)
+  font_type = "Helvetica"
 
+  # pdf.stroke_axis
+
+  pdf.move_down 20
+  pdf.font(font_type, :size => 12) { pdf.text story } # story
+  pdf.move_down 20
+  pdf.font(font_type, :size => 24, :style => :bold) { pdf.text task } # task
+
+  if (estimate) # have an hours estimate for the task?
+    pdf.bounding_box([325, 22], :width => 40, :height => 12) do
+      pdf.font(font_type, :size => 10, :style => :italic) { pdf.text estimate, :valign => :bottom, :align => :right }
+      # stroke_bounds
+    end
+  end
+
+  pdf.bounding_box([330,230], :width => 50, :height => 50) do
+    pdf.icon icon, size:30
+    # stroke_bounds
+  end
+
+  pdf.start_new_page
 end
 
-pdf = Prawn::Document.generate( "cards.pdf", :page_size => [432, 288]) do
+Prawn::Document.generate( "cards.pdf", :page_size => [432, 288]) do |pdf|
 
   stories.each do |story|
     card_pic = icons.shift
     # puts card_pic
     if (story.tasks.empty?)
       puts "Story with no tasks: #{story.name}"
-      # puts story.name
+      gen_card(pdf, card_pic, story.name, story.name, nil)
     end
     story.tasks.each do |task|
 
-      # stroke_axis
-
       /(.+?)( \((\d+h)\))?$/ =~ task.description
 
-      move_down 20
-      font(font_type, :size => 12) { text story.name } # story
-      move_down 20
-      font(font_type, :size => 24, :style => :bold) { text $1 } # task
+      gen_card( pdf, card_pic, story.name, $1, $3)
 
-      if ($3) # have an hours estimate for the task?
-        bounding_box([325, 22], :width => 40, :height => 12) do
-          font(font_type, :size => 10, :style => :italic) { text $3, :valign => :bottom, :align => :right }
-          # stroke_bounds
-        end
-      end
-
-      bounding_box([330,230], :width => 50, :height => 50) do
-        icon card_pic, size:30
-        # stroke_bounds
-      end
-
-      start_new_page
     end
   end
 end
