6e05b1e7fc7cbe55231e84040e6e24afb13c137e
Saadiq Rodgers-King <saadiq@gmail.com>
2017-02-06 17:27:41 -0500

    about refactor to print cards for stories without task
    committing in case I need to come back to a working version

--- pivotal_task_cards.rb
+++ pivotal_task_cards.rb
@@ -12,7 +12,8 @@ font_type = "Helvetica"
 icons = [ 'fa-shopping-bag', 'fa-ticket', 'fa-map-pin', 'fa-rocket',
   'fa-car', 'fa-anchor', 'fa-camera', 'fa-bullhorn', 'fa-bullseye',
   'fa-asterisk', 'fa-bicycle', 'fa-cloud', 'fa-diamond', 'fa-flag',
-  'fa-microphone', 'fa-plane' ]
+  'fa-plane', 'fa-cogs', 'fa-magnet', 'fa-paper-plane', 'fa-hashtag',
+  'fa-plug', 'fa-shopping-cart', 'fa-moon-o', 'fa-wrench']
 
 client = TrackerApi::Client.new(token: @conf['pivotal_api_key'])
 project = client.project(1916005) # dev project
@@ -23,11 +24,19 @@ stories = project.stories(filter: 'label:"sprint 5"')
 icons.shuffle! # don't want to get bored with the icons
 card_pic = icons[0]
 
+def gen_card(pdf, icon, story, task, estimate)
+
+end
+
 pdf = Prawn::Document.generate( "cards.pdf", :page_size => [432, 288]) do
 
   stories.each do |story|
     card_pic = icons.shift
-
+    # puts card_pic
+    if (story.tasks.empty?)
+      puts "Story with no tasks: #{story.name}"
+      # puts story.name
+    end
     story.tasks.each do |task|
 
       # stroke_axis
